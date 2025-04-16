21d9590c3a900d93da969e5672a0fd9a357f1ec6
alej <markup-imago0s@icloud.com>
2025-04-16 10:09:56 -0400

    actualizacion final, proytecto en aire

--- Lector Driver Id/Lector Driver ID Oficial/main.py
+++ Lector Driver Id/Lector Driver ID Oficial/main.py
@@ -1,9 +1,11 @@
 import sys
+from PyQt6.QtGui import QIcon
 from PyQt6.QtWidgets import QApplication
 from interfaz import SerialReaderApp
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
+    app.setWindowIcon(QIcon("ico.ico"))
     window = SerialReaderApp()
     window.show()
     sys.exit(app.exec())
